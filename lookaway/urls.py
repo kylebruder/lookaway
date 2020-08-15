3789634e92ac877d05e0171866054019676eecf3
Kyle Bruder <kylebruder@pm.me>
2020-08-15 20:42:59 +0000

    add password change and reset views

--- lookaway/urls.py
+++ lookaway/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 from django.urls import include, path
 from . import settings
@@ -35,6 +36,41 @@ urlpatterns = [
     path(
         '',
         include('home.urls'),
+    ),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='login.html'),
+        name='login',
+    ),
+    path(
+        'logout',
+         auth_views.LogoutView.as_view(),
+         name='logout',
+    ),
+    path(
+        'change-password/',
+         auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+         name='password_change',
+    ),
+    path(
+        'reset-password/',
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+         name='password_reset',
+    ),
+    path(
+        'reset-password-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset.html'),
+         name='password_reset_confirm',
+    ),
+    path(
+        'reset-password-done',
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name='password_reset_done',
+    ),
+    path(
+        'reset-password-complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete',
     ),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- members/urls.py
+++ members/urls.py
@@ -6,16 +6,6 @@ app_name = "members"
 
 urlpatterns = [
     path(
-        'login/', 
-        auth_views.LoginView.as_view(template_name='members/login.html'),
-        name='login',
-    ),
-    path(
-        'logout',
-         auth_views.LogoutView.as_view(),
-         name='logout',
-    ),
-    path(
         'studio/',
          StudioView.as_view(),
          name='studio',
