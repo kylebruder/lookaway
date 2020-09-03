49c82e17658ad93b5d3ad9859a7ed2965eb21583
Kyle Bruder <kylebruder@pm.me>
2020-09-03 22:31:09 +0000

    fix for backup script

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BUCKET="lookaway"
-tar cp /home/lookaway/lookaway-env/lookaway/media | gzip | aws s3 cp - "s3://$BUCKET/backups/$(date '+20%y-%m-%d')/media.tar.gz"
-sudo -u postgres pg_dump lookaway > /tmp/lookaway.sql
-gzip /tmp/lookaway.sql
-aws s3 cp /tmp/lookaway.sql.gz "s3://$BUCKET/backups/$(date '+20%y-%m-%d')/lookaway.sql.gz"
-rm /tmp/lookaway.sql.gz
+/bin/tar cp /home/lookaway/lookaway-env/lookaway/media | gzip | aws s3 cp - "s3://$BUCKET/backups/$(date '+20%y-%m-%d')/media.tar.gz"
+/usr/bin/sudo -u postgres pg_dump lookaway > /tmp/lookaway.sql
+/bin/gzip /tmp/lookaway.sql
+/usr/local/bin/aws s3 cp /tmp/lookaway.sql.gz "s3://$BUCKET/backups/$(date '+20%y-%m-%d')/lookaway.sql.gz"
+/bin/rm /tmp/lookaway.sql.gz
