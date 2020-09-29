fe2fb32543eae262ad3b5747a5c87c10d715c9fc
Kyle Bruder <kyle@kbruder.info>
2020-09-29 20:13:00 +0000

    attempted fix for music Marshmallows

--- music/urls.py
+++ music/urls.py
@@ -98,12 +98,12 @@ urlpatterns = [
     ),
     # Marshmallow Views
     path(
-        'track/<int:pk>/add-marshmallow/', 
+        'track/<int:pk>/add-marshmallow', 
         views.add_marshmallow_to_album_view,
         name='album_marshmallow',
     ),
     path(
-        'track/<int:pk>/add-marshmallow/', 
+        'track/<int:pk>/add-marshmallow', 
         views.add_marshmallow_to_track_view,
         name='track_marshmallow',
     ),
