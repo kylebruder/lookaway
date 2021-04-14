1c069e03d4a7018cb141a772ba8d05d84ddd9ac0
Kyle Bruder <kylebruder@pm.me>
2021-04-14 07:58:42 -0700

    add Members app URLs

--- members/urls.py
+++ members/urls.py
@@ -1,37 +1,69 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import StudioView, MemberListView, MemberProfileView, MemberProfileUpdateView, InviteLinkDetailView, member_registration
+import members.views as views
 
 app_name = "members"
 urlpatterns = [
+    # Members app profile
+    ## Update
+    path(
+        'profile/update/<int:pk>/', 
+        views.MembersAppProfileUpdateView.as_view(),
+        name='members_app_profile_update',
+    ),
+    # Members landing page section views
+    ## Create
+    path(
+        'add/section/', 
+        views.MembersPageSectionCreateView.as_view(),
+        name='members_page_section_create',
+    ),
+    ## Detail
+    path(
+        'sections/<int:pk>/',
+        views.MembersPageSectionDetailView.as_view(),
+        name='members_page_section_detail',
+    ),
+    ## Update
+    path(
+        'update/section/<int:pk>/', 
+        views.MembersPageSectionUpdateView.as_view(),
+        name='members_page_section_update',
+    ),
+    ## Delete
+    path(
+        'delete/section/<int:pk>/',
+        views.MembersPageSectionDeleteView.as_view(),
+        name='members_page_section_delete',
+    ),
     path(
         '', 
-        MemberListView.as_view(),
+        views.MemberListView.as_view(),
         name='member_list',
     ),
     path(
         'studio/',
-        StudioView.as_view(),
+        views.StudioView.as_view(),
         name='studio',
     ),
     path(
         '<slug:slug>/', 
-        MemberProfileView.as_view(),
+        views.MemberProfileView.as_view(),
         name='member_profile',
     ),
     path(
         '<slug:slug>/profile/', 
-        MemberProfileUpdateView.as_view(),
+        views.MemberProfileUpdateView.as_view(),
         name='member_profile_update',
     ),
     path(
         'join-us/<slug:slug>/', 
-        InviteLinkDetailView.as_view(),
+        views.InviteLinkDetailView.as_view(),
         name='invite_link_detail',
     ),
     path(
         'register/<slug:invite>/', 
-        member_registration,
+        views.member_registration,
         name='member_registration',
     ),
 ]
