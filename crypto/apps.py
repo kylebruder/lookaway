66799f843088cade15d1b9501e4e1f0eb80025d2
Kyle Bruder <kylebruder@pm.me>
2021-06-01 16:40:02 -0700

    fixes for new version of Django

--- crypto/apps.py
+++ crypto/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class CryptoConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'crypto'
 
     def ready(self):
--- objects/apps.py
+++ objects/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class ObjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'objects'
 
     def ready(self):
